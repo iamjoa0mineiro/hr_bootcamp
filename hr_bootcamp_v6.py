a4eed2fa7340cc51941aa2afa1f13dedfc2abda2
brunotereso <165080255+brunotereso@users.noreply.github.com>
2024-11-29 17:16:15 +0000

    Updated TO DO

--- hr_bootcamp_v6.py
+++ hr_bootcamp_v6.py
@@ -406,7 +406,7 @@ X = hr.drop(columns=['Attrition'])
 y = hr['Attrition']
 
 # Split into Train and Test Sets (70% training, 30% testing)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=42)
 
 # Set global random state for reproducibility
 r_state = 99
